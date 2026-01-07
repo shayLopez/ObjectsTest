9720863cb8fbdac7621f8209a7b5afe08ad9d2bc
shayLopez <shay0584751725@gmail.com>
2026-01-07 22:07:53 +0200

    update1

--- app/src/main/java/com/example/objectstest/Intake.java
+++ app/src/main/java/com/example/objectstest/Intake.java
@@ -1,6 +1,12 @@
 package com.example.objectstest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Intake extends AppCompatActivity {
+    Switch areaordiam;
+    Button next;
+    Intent go;
+
+RadioButton B1,B2,B3,B4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,23 @@ public class Intake extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        intitcomp();
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public  void intitcomp()
+    {
+        B1=findViewById(R.id.radioButton);
+        B2=findViewById(R.id.radioButton2);
+        B3=findViewById(R.id.radioButton3);
+        B4=findViewById(R.id.radioButton4);
+        areaordiam=findViewById(R.id.switch1);
+        next=findViewById(R.id.button2);
+        go=new Intent(Intake.this,Test.class);
     }
 }
